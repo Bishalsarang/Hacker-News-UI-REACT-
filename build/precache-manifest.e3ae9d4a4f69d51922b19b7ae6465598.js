0016f8606d23253de065fb3d210d7cdc001080f8
Bishalsarang <bishalsarang@gmail.com>
2020-08-03 10:20:14 +0545

    FIx homepage in package.json

---
+++ build/precache-manifest.e3ae9d4a4f69d51922b19b7ae6465598.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7ae4c1e8f6a907f2df06cef6063f476",
+    "url": "/Hacker-News-UI-REACT-/build/index.html"
+  },
+  {
+    "revision": "a04f8ad0e693c37ee1fd",
+    "url": "/Hacker-News-UI-REACT-/build/static/css/main.98d8fa8b.chunk.css"
+  },
+  {
+    "revision": "955ea1929b629f3cd588",
+    "url": "/Hacker-News-UI-REACT-/build/static/js/2.88c003d0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Hacker-News-UI-REACT-/build/static/js/2.88c003d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a04f8ad0e693c37ee1fd",
+    "url": "/Hacker-News-UI-REACT-/build/static/js/main.9c5fb811.chunk.js"
+  },
+  {
+    "revision": "134dcc7805fbc07ebb47",
+    "url": "/Hacker-News-UI-REACT-/build/static/js/runtime-main.5fd73f51.js"
+  },
+  {
+    "revision": "236d70a3fdbae4ad1df5397985d70acb",
+    "url": "/Hacker-News-UI-REACT-/build/static/media/loader.236d70a3.svg"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e72d8433270ff5743fbef7e7fbe4302a.js"
+  "/Hacker-News-UI-REACT-/build/precache-manifest.e3ae9d4a4f69d51922b19b7ae6465598.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Hacker-News-UI-REACT-/build/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
