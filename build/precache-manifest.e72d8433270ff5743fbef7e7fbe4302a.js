499ec9ce45b19ce19d3a77905499ebfb213490d7
Bishalsarang <bishalsarang@gmail.com>
2020-08-03 09:51:10 +0545

    Add build

---
+++ build/precache-manifest.e72d8433270ff5743fbef7e7fbe4302a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e2ce55bd79fa890d778193dbff47f7f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c49f9d1674578472f9fa",
+    "url": "/static/css/main.98d8fa8b.chunk.css"
+  },
+  {
+    "revision": "955ea1929b629f3cd588",
+    "url": "/static/js/2.88c003d0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.88c003d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c49f9d1674578472f9fa",
+    "url": "/static/js/main.a72306fb.chunk.js"
+  },
+  {
+    "revision": "6aba926dd4466eb47ee0",
+    "url": "/static/js/runtime-main.080beaec.js"
+  },
+  {
+    "revision": "236d70a3fdbae4ad1df5397985d70acb",
+    "url": "/static/media/loader.236d70a3.svg"
+  }
+]);
